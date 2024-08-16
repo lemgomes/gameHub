dfe08342b0190f0f1c24bb291709256ed5c21dfa
lgomes <lgomes@springglobal.com>
2024-08-16 11:35:13 -0300

    Refactor Game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,10 +12,9 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data: games, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+  if (error) return <Text>{error}</Text>;
   return (
     <>
-      <Text>{isLoading}</Text>
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         p={"10px"}
