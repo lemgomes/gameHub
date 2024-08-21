13a18b1c0da5d5b9d3e4979570cfc3dcf4085b63
lgomes <lgomes@springglobal.com>
2024-08-21 09:46:03 -0300

    Refactor Simplify time calculation

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,6 @@
 import APIClient from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 
 const apiClient = new APIClient<Genre>("/genres");
 export interface Genre {
@@ -12,7 +13,7 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24 hs
+    staleTime: ms("24h"), //24 hs
   });
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,6 @@
 import APIClient from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
@@ -13,7 +14,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24 hs
+    staleTime: ms("24h"), //24 hs
   });
 
 export default usePlatforms;
